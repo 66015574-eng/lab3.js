56a5c36d60995ae9eefa1de5d18053efc4c4952f
66015574-eng <66015574@rbac.ac.th>
2025-11-11 12:38:06 +0700

    Initial commit

---
+++ lab3.js
@@ -0,0 +1,22 @@
+let fName = "yet";
+var IName = "yark";
+const weight =10.5;
+
+
+
+
+console.log(fname);
+console.log(Iname);
+
+{
+    let a = 100;
+    var b = 200;
+    console.log(fName);
+    console.log(IName);
+    console.log(a);
+    console.log(b);
+}
+ console.log(fName);
+    console.log(IName);
+    console.log(b);
+
